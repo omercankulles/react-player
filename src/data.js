39e5baf2a5abfc337401d350179ab83200a7cdce
omercankulles <you@example.com>
2021-07-19 11:47:37 +0300

    init

---
+++ src/data.js
@@ -0,0 +1,89 @@
+import {v4 as uuidv4} from 'uuid';
+
+function chillHop(){
+    return [
+        {
+            name: "LuvnYou",
+            cover : "https://chillhop.com/wp-content/uploads/2021/07/875b9624506ae8d05750b9e4ab4579abb1fe3e16-1024x1024.jpg",
+            artist : "Ian Ewing",
+            audio : "https://mp3.chillhop.com/serve.php/?mp3=21784",
+            color : ["#205950","#2ab3bf"],
+            id : uuidv4(),
+            active: true,
+        },
+        {
+            name: "Dawn",
+            cover : "https://chillhop.com/wp-content/uploads/2021/05/732128e1da8fd5f6292ffc1926a2ea840a54f4d0-150x150.jpg",
+            artist : "Toonorth",
+            audio : "https://mp3.chillhop.com/serve.php/?mp3=17941",
+            color : ["#656565","#A6D3A0"],
+            id : uuidv4(),
+            active: false,
+        },
+        {
+            name: "Cruising",
+            cover : "https://chillhop.com/wp-content/uploads/2021/04/cb0cc6270d7f2e1bb13e44e8832bd5c9b2a61080-150x150.jpg",
+            artist : "Evil Needle",
+            audio : "https://mp3.chillhop.com/serve.php/?mp3=17087",
+            color : ["#8ACDEA","#746D75"],
+            id : uuidv4(),
+            active: false,
+        },
+        {
+            name: "Far From Home",
+            cover : "https://chillhop.com/wp-content/uploads/2020/12/7e98d3028a22ee7f16f6a9bfcdc2089f089777a5-150x150.jpg",
+            artist : "Toonorth",
+            audio : "https://mp3.chillhop.com/serve.php/?mp3=8127",
+            color : ["#628395","#FFA0A0"],
+            id : uuidv4(),
+            active: false,
+        },
+        {
+            name: "Plants",
+            cover : "https://chillhop.com/wp-content/uploads/2020/09/2899f7cc22ab12e17d0119819aac3ca9dbab46e6-150x150.jpg",
+            artist : "Philanthrope, Ian Ewing, Sleepy Fish",
+            audio : "https://mp3.chillhop.com/serve.php/?mp3=10255",
+            color : ["#205950","#2ab3bf"],
+            id : uuidv4(),
+            active: false,
+        },
+        {
+            name: "Habitat",
+            cover : "https://chillhop.com/wp-content/uploads/2021/02/7f102bdde417f6ead9a120b2b931449e5c12b4da-150x150.jpg",
+            artist : "Philanthrope, Kendall Miles",
+            audio : "https://mp3.chillhop.com/serve.php/?mp3=10265",
+            color : ["#205950","#2ab3bf"],
+            id : uuidv4(),
+            active: false,
+        },
+        {
+            name: "Kinsfolk",
+            cover : "https://chillhop.com/wp-content/uploads/2021/02/70a56749b8b89815fa75446030c6ba57d2c34de7-150x150.jpg",
+            artist : "Aarigod",
+            audio : "https://mp3.chillhop.com/serve.php/?mp3=12997",
+            color : ["#205950","#2ab3bf"],
+            id : uuidv4(),
+            active: false,
+        },
+        {
+            name: "Déjà Vu",
+            cover : "https://chillhop.com/wp-content/uploads/2020/12/33a2a875828118a3ff260638a88362936104879a-150x150.jpg",
+            artist : "Blue Wednesday, Shopan",
+            audio : "https://mp3.chillhop.com/serve.php/?mp3=11229",
+            color : ["#205950","#2ab3bf"],
+            id : uuidv4(),
+            active: false,
+        },
+        {
+            name: "False Hope",
+            cover : "https://chillhop.com/wp-content/uploads/2020/12/7e98d3028a22ee7f16f6a9bfcdc2089f089777a5-1024x1024.jpg",
+            artist : "Nymano, Pandrezz",
+            audio : "https://mp3.chillhop.com/serve.php/?mp3=10148",
+            color : ["#205950","#2ab3bf"],
+            id : uuidv4(),
+            active: false,
+        },
+    ];
+}
+
+export default chillHop;
